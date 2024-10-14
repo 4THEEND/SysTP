c3e11ca467a64ec14b0add5b0bc4f1723c80cd63
Ismama <gayeismael22@gmail.com>
2024-10-14 16:12:33 +0200

    modified:   src/stack.h

--- src/stack.h
+++ src/stack.h
@@ -8,11 +8,11 @@ typedef struct
     char m_stack[NB_JOUEURS_MAX];
     int m_pos_stack = 0;
 
-    void push(char elt);
-    char pop();
-    char peek();
-    char top();
-    int height();
+    void push(stack* pile, char elt);
+    char pop(stack* pile);
+    char peek(stack* pile);
+    char top(stack* pile);
+    int height(stack* pile);
 
 } stack;
 
