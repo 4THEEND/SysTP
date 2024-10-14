d71f624f9d458f673dad32d451688a6bdd4b7357
Ismama <gayeismael22@gmail.com>
2024-10-14 11:12:31 +0200

    modified:   src/board.c

--- src/board.c
+++ src/board.c
@@ -1,1 +1,29 @@
 #include "board.h"
+
+void board_push(board_t* b, int line, int row, char ctn){
+
+}
+
+char board_pop(board_t* b, int line, int row){
+    return 'a';
+}
+
+int board_height(board_t* b, int line, int row){
+    return 0;
+}
+
+char board_top(board_t* b, int line, int row){
+    return 'a';
+}
+
+char board_peek(board_t* b, int line, int row, int pos){
+    return 'a';
+}
+
+void cell_print(board_t* b, int line, int row, int slice){
+    
+}
+
+void board_print(board_t* b, int highlighted_line){
+    
+}
