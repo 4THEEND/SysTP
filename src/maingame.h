d3285c30d678c994f5d322178365de27b90a17d5
Fossam40 <nocubesp@gmail.com>
2024-10-15 18:42:03 +0200

    modified:   src/maingame.h

--- src/maingame.h
+++ src/maingame.h
@@ -1,4 +1,9 @@
 #ifndef MAINGAME_H
 #define MAINGAME_H
 
+int de();
+int get_winner(int* herissonFinis);
+bool is_coordinate_valid(int colonne, int ligne);
+bool is_ligne_vide(board_t* b, int ligne);
+int play_game(board_t* b);
 #endif
