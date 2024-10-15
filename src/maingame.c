8c00c32c8d4a40b9f593995cec732a60899fcea2
Fossam40 <nocubesp@gmail.com>
2024-10-15 18:39:41 +0200

    modified:   src/maingame.c

--- src/maingame.c
+++ src/maingame.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 
 int de(){
-    return (rand() % NB_LINE)
+    return (rand() % NB_LINE);
 }
 
 int get_winner(int* herissonsFinis){ //retourne -1 s'il n'y a pas de gagnant
@@ -12,18 +12,18 @@ int get_winner(int* herissonsFinis){ //retourne -1 s'il n'y a pas de gagnant
             return i;
         }
     }
-    return i;
+    return -1;
 }
 
 bool is_coordinate_valid(int colonne, int ligne){
-    bool is_colonne_valid = (colonne >= 1) && (colonne <= NB_COL);
+    bool is_colonne_valid = (colonne >= 1) && (colonne <= NB_ROW);
     bool is_ligne_valid = (ligne >= 1 ) && (ligne <= NB_LINE);
     return is_colonne_valid && is_ligne_valid;
 }
 
 bool is_ligne_vide(board_t* b, int ligne){
-    is_empty = true;
-    for(int i = 0; i < NB_COL - 1; i++){
+    bool is_empty = true;
+    for(int i = 0; i < NB_ROW - 1; i++){
         if(board_height(b, ligne, i) != 0){
             is_empty = false;
         }
@@ -36,7 +36,7 @@ int play_game(board_t* b){ //retourne le joueur gagnant
     for(int i = 0; i< NB_JOUEURS; i++){
         herissonsFinis[i] = 0;
     }
-    int winner = -1:                                                    
+    int winner = -1;                                                
     while(winner = -1){
         for(int joueur = 0; joueur < NB_JOUEURS; joueur++){
             int resultat_de = de();
@@ -53,14 +53,13 @@ int play_game(board_t* b){ //retourne le joueur gagnant
                 int ligne_herisson;
                 int col_herisson;
                 int ligne_objectif;
-                printf("Indiquez la ligne, puis la colonne du 
-                herisson que vous souhaitez changer de ligne, ainsi que la ligne ligne_objectif. \n ");
+                printf("Indiquez la ligne, puis la colonne du herisson que vous souhaitez changer de ligne, ainsi que la ligne ligne_objectif. \n ");
                 printf("Par exemple, pour déplacer le herisson dans la case (3,2) dans la ligne 4, on écrit 3 2 4. \n");
-                scanf("%d %d %d", ligne_herisson, col_herisson, ligne_objectif);
+                scanf("%d %d %d", &ligne_herisson, &col_herisson, &ligne_objectif);
                 while( !(is_coordinate_valid(ligne_herisson, col_herisson)) || !(is_coordinate_valid(ligne_objectif - 1, 0)) 
                     || board_top(b, ligne_herisson - 1, col_herisson - 1) != (char)(joueur + (int)'a') ){
                     printf("Veuillez à donner des coordonnées correctes, et choisir un de vos hérissons. \n");
-                    scanf("%d %d %d", ligne_herisson, col_herisson, ligne_objectif);
+                    scanf("%d %d %d", &ligne_herisson, &col_herisson, &ligne_objectif);
                 }
                 char ignore = board_pop(b, ligne_herisson - 1, col_herisson - 1);
                 board_push(b, ligne_objectif - 1, col_herisson - 1, (char)((int)'a' + joueur));
@@ -71,14 +70,14 @@ int play_game(board_t* b){ //retourne le joueur gagnant
             else{
                 int colonne_deplace;
                 printf("Choisissez une colonne: ");
-                scanf("%d", colonne_deplace);
-                while(!(is_coordinate_valid(b, 0, colonne_deplace)) || board_height(b, resultat_de, colonne_deplace - 1) == 0){
+                scanf("%d", &colonne_deplace);
+                while(!(is_coordinate_valid(0, colonne_deplace)) || board_height(b, resultat_de, colonne_deplace - 1) == 0){
                     printf("Veuillez saisir une colonne valide non vide: \n");
-                    scanf("%d", colonne_deplace);
+                    scanf("%d", &colonne_deplace);
                 }
                 char player = board_pop(b, resultat_de, colonne_deplace - 1);
                 board_push(b, resultat_de, colonne_deplace - 1, player);
-                if(colonne_deplace == NB_COL){
+                if(colonne_deplace == NB_ROW){
                     herissonsFinis[(int)(player - (char)'a')]++;
                 }
             }
