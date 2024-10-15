23c5024f38afce9d469413ef21b2db2611b02362
Ismama <gayeismael22@gmail.com>
2024-10-15 16:29:40 +0200

    modified:   src/g_interface.c 	modified:   src/g_interface.h

--- src/g_interface.c
+++ src/g_interface.c
@@ -1,11 +1,24 @@
 #include "g_interface.h"
 #include <SDL2/SDL.h>
+#include <stdbool.h>
+
 
 void run_interface(){
     if(SDL_Init(SDL_INIT_GAMECONTROLLER | SDL_INIT_VIDEO | SDL_INIT_EVENTS) != 0){
-        printf("[*] Failed initialisation of SDL2\n");
+        fprintf(stderr, "[*] Failed to intialise SDL2: %s", SDL_GetError());
     }
-    printf("[*] Initialisation of SDL2\n");
+    printf("[*] Sucessfully initialised SDL2\n");
+
+    SDL_Window* window = SDL_CreateWindow("Igel Ärgern", 0, 0 , WINDOW_WIDTH, WINDOW_HEIGHT, 0);
+    if (window == NULL){
+        fprintf(stderr, "[*] Failed to create the window with error: %s", SDL_GetError());
+    }
+
+    while(true){
+        SDL_ShowWindow(window);
+    }
+
+    printf("[*] Window exited sucessfully!!\n");
 
     SDL_Quit();
 }
--- src/g_interface.h
+++ src/g_interface.h
@@ -1,6 +1,9 @@
 #ifndef G_INTERFACE_H
 #define G_INTERFACE_H
 
+#define WINDOW_WIDTH 200
+#define WINDOW_HEIGHT 200
+
 void run_interface();
 
 #endif
