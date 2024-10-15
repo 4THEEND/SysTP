f240222253710b21431b640b5e25602634cab389
Ismama <gayeismael22@gmail.com>
2024-10-15 15:56:47 +0200

    modified:   build/Makefile 	new file:   build/prog_test 	new file:   src/test.c

---
+++ src/test.c
@@ -0,0 +1,17 @@
+#include "stack.h"
+#include <assert.h>
+#include <stdio.h>
+
+
+void test_stack(void){
+    stack_t s1 = {.m_pos = -1};
+    push(&s1, 'c');
+    assert(top(&s1) == 'c');
+}
+
+int main(){
+    test_stack();
+
+    printf("Tous les test se sont bien déroulés :)\n");
+    return 0;
+}
