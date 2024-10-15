add4ea170eddc036afdd547968e628054fe3923c
Fossam40 <nocubesp@gmail.com>
2024-10-15 15:44:18 +0200

    modified:   src/main.c

--- src/main.c
+++ src/main.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 #include "board.h"
+#include <time.h>
+#include <stdlib.h>
+
 
 int main(int argc, char** argv){
+    srand(time(NULL));
     board_t board;
     init_board(&board);
     board_print(&board, 2);
