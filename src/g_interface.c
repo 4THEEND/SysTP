e1fe150e538e06c9b31eb3cb8dc2a7f1f2918114
Ismama <gayeismael22@gmail.com>
2024-10-16 19:53:58 +0200

    load all hedgehog except the pink one into memory

--- src/g_interface.c
+++ src/g_interface.c
@@ -3,9 +3,11 @@
 #include <SDL2/SDL.h>
 #include <stdbool.h>
 
-void exit_sdl(SDL_Surface* board_img){
-    if (board_img != NULL)
-        SDL_FreeSurface(board_img);
+void exit_sdl(int nb_free, SDL_Surface* to_free[]){
+    for (int i = 0; i < nb_free; i++){
+        if (to_free[i] != NULL)
+            SDL_FreeSurface(to_free[i]);
+    }
     SDL_Quit();
     exit(EXIT_SUCCESS);
 }
@@ -20,22 +22,52 @@ void run_game(){
     SDL_Window* window = SDL_CreateWindow("Igel Ärgern", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WINDOW_WIDTH, WINDOW_HEIGHT, 0);
     if (window == NULL){
         fprintf(stderr, "[*] Failed to create the window with error: %s\n", SDL_GetError());
-        exit_sdl(NULL);
+        exit_sdl(0, NULL);
     }
 
     SDL_ShowWindow(window);
 
-    SDL_Surface* board_image = SDL_LoadBMP(BOARD_BMP_PATH);
-    if (board_image == NULL){
+    SDL_Surface* images_tab[NB_IMAGES] = {NULL, NULL, NULL, NULL, NULL};
+    /*In images tab we have 
+    0 : board image
+    1 : red hedgehog image
+    2 : blue hedgehog image
+    3 : green hedgehog image
+    4 : pink ? hedgehog image
+    */
+
+    images_tab[0] = SDL_LoadBMP(BOARD_BMP_PATH);
+    if (images_tab[0] == NULL){
         fprintf(stderr, "[*] Failed to open the board image with error : %s\n", SDL_GetError());
-        exit_sdl(NULL);
+        exit_sdl(0, NULL);
     }
     printf("[*] Sucessfully opened the board image\n");
 
+    images_tab[1] = SDL_LoadBMP(RED_HG_BMP_PATH);
+    if (images_tab[1] == NULL){
+        fprintf(stderr, "[*] Failed to open the red hedgehog image with error : %s\n", SDL_GetError());
+        exit_sdl(NB_IMAGES, images_tab);
+    }
+    printf("[*] Sucessfully opened the red hedgehog image\n");
+
+    images_tab[2] = SDL_LoadBMP(BLUE_HG_BMP_PATH);
+    if (images_tab[2] == NULL){
+        fprintf(stderr, "[*] Failed to open the blue hedgehog image with error : %s\n", SDL_GetError());
+        exit_sdl(NB_IMAGES, images_tab);
+    }
+    printf("[*] Sucessfully opened the blue hedgehog image\n");
+
+    images_tab[3] = SDL_LoadBMP(GREEN_HG_BMP_PATH);
+    if (images_tab[3] == NULL){
+        fprintf(stderr, "[*] Failed to open the green hedgehog image with error : %s\n", SDL_GetError());
+        exit_sdl(NB_IMAGES, images_tab);
+    }
+    printf("[*] Sucessfully opened the green hedgehog image\n");
+
     SDL_Rect board_pos = {0, 0, 0, 0};
-    if (SDL_BlitSurface(board_image, NULL, SDL_GetWindowSurface(window), &board_pos) != 0){
+    if (SDL_BlitSurface(images_tab[0], NULL, SDL_GetWindowSurface(window), &board_pos) != 0){
         fprintf(stderr, "[*] Failed to bind the board image to the window : %s\n", SDL_GetError());
-        exit_sdl(NULL);
+        exit_sdl(NB_IMAGES, images_tab);
     }
 
     SDL_Event event;
@@ -63,7 +95,6 @@ void run_game(){
                     quit = true;
                     break;
             }
-            SDL_Delay(1000);
         }
         SDL_UpdateWindowSurface(window);
     }   
@@ -73,7 +104,7 @@ void run_game(){
 
     SDL_DestroyWindow(window);
     printf("[*] Window exited sucessfully!!\n");
-    exit_sdl(board_image);
+    exit_sdl(NB_IMAGES, images_tab);
 }
 
 
--- src/g_interface.h
+++ src/g_interface.h
@@ -3,10 +3,18 @@
 
 #include "board.h"
 
+#define NB_IMAGES 5
+
 #define WINDOW_WIDTH 1500
 #define WINDOW_HEIGHT 1032
 
+#define HEDGEHOG_WIDTH 105
+#define HEDGEHOG_HEIGHT 115
+
 #define BOARD_BMP_PATH "../images/board.bmp"
+#define RED_HG_BMP_PATH "../images/red.bmp"
+#define BLUE_HG_BMP_PATH "../images/blue.bmp"
+#define GREEN_HG_BMP_PATH "../images/green.bmp"
 
 
 
